65bc7b9a9c84ff060aded32ea097b26e99717e5e
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-01-04 00:04:49 +0300

    Add ProcessEnv interface for node environment variables

---
+++ src/types/environment.d.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars
+import * as ts from 'typescript';
+// Note: the snippet below is module augmentation.Files containing module
+// augmentation must be modules(as opposed to scripts).The difference between
+// modules and scripts is that modules have at least one import /export statement.
+
+// In order to make TypeScript treat your file as a module, just add one import
+// statement to it.It can be anything.Even import * as ts from 'typescript' will do.
+
+// https://stackoverflow.com/a/53981706/2015025
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DESTINATION_BUCKET: string;
+      DESTINATION_BUCKET_ACL: string;
+    }
+  }
+}
